80d4e7db076bc6288f6acebd9ecd95da9bd4acb3
kumarshreyak@gmail.com <shreyak.kumar@novopay.in>
2020-07-01 20:22:00 +0530

    Added data binding.

--- app/src/main/java/com/shrekiscool/githubapp/ui/MainActivity.kt
+++ app/src/main/java/com/shrekiscool/githubapp/ui/MainActivity.kt
@@ -2,12 +2,16 @@ package com.shrekiscool.githubapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.shrekiscool.githubapp.R
+import com.shrekiscool.githubapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
